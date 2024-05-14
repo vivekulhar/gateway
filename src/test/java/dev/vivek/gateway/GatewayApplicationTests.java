9c5e9791600fc48ac910e2f81615bb4f0422fbaa
vivekulhar <105244989+vivekulhar@users.noreply.github.com>
2024-05-14 12:54:12 +0530

    Initial commit

---
+++ src/test/java/dev/vivek/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vivek.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
